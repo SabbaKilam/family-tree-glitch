d46f7c06f75d2938ff924d917a8e8c82142ff1ba
Glitch (able-coal) <none>
2019-01-31 21:23:54 +0000

    🐑💃 Checkpoint ./.glitch-assets:243166/465 ./style.css:243166/2

--- components/Cameo.js
+++ components/Cameo.js
@@ -1,79 +1,98 @@
 /*global React*/
 class Cameo extends React.Component{
-  constructor( props ) {		
+	constructor( props ) {		
 		super( props );
-    this.state = {
-      normalSize: "7rem",
-      isSelected: false,
-      percentViewport: 30
-    }
-    
-    this['enlargeCameo'] = this.enlargeCameo.bind(this);
-    this.restoreCameoSize = this.restoreCameoSize.bind(this);    
-    
+		this.state = {
+		  normalSize: "7rem",
+		  isSelected: false,
+		  percentViewport: 18,
+		  nameHolder: document.querySelector('#nickName'),
+		  infoHolder: document.querySelector('#infoHolder'),
+		  infoHolderCameo: document.querySelector("#infoHolderCameo"),
+		  infoHolderInfo: document.querySelector("#infoHolderInfo")
+		  
+		  //id="infoHolderCameo"
+		};
+		
+		this.enlargeCameo = this.enlargeCameo.bind(this);
+		this.restoreCameoSize = this.restoreCameoSize.bind(this);
 	}
 	////////////////////////////////////////////////////////////////////
   enlargeCameo(eventObject){
+	//let image = "../" + this.props.info.portraitURL.slice(2);
+	let image = this.props.info.portraitURL.slice(2);
+	let cameo = this.state.infoHolderCameo;
+	let infoHolderInfo = this.state.infoHolderInfo;
 
-
-    clearInterval(this.state.timerId)
+	this.state.nameHolder.innerText = this.props.name;
+	//infoHolder.innerText = this.props.info.name;
+	cameo.style.background = `url(${image}) no-repeat center`;
+	cameo.style.backgroundSize = "cover";	
+	infoHolderInfo.innerText = `${this.props.info.name}\n${this.props.info.nickName ? this.props.info.nickName : ""}\n${this.props.info.email}`;	
+	
+	// no-repeat center
+	//alert(this.props.info.name);
+	
+    clearInterval(this.state.timerId);
     const source = eventObject.target;
       const viewportUnit = window.innerWidth >= window.innerHeight ?
-                            "vw"
-                          : "vh"        
+                            "vh"
+                          : "vw" 
+      ;
     //alert(eventObject.target.id)
-    source.style.height = `${this.state.percentViewport}${viewportUnit}`
-    source.style.width = `${this.state.percentViewport}${viewportUnit}`
+    source.style.height = `${this.state.percentViewport}${viewportUnit}`;
+    source.style.width = `${this.state.percentViewport}${viewportUnit}`;
     this.setState({
       isSelected: true,
-     })
+     });
     this.setState({
       timerId: (
       		setInterval(()=>{
             if ( this.state.isSelected ){
                 const viewportUnit = window.innerWidth >= window.innerHeight ?
                             "vw"
-                          : "vh"  
-                source.style.height = `${this.state.percentViewport}${viewportUnit}`
-                source.style.width = `${this.state.percentViewport}${viewportUnit}`            
+                          : "vh"
+            	;
+                source.style.height = `${this.state.percentViewport}${viewportUnit}`;
+                source.style.width = `${this.state.percentViewport}${viewportUnit}`;            
             }
             else if ( !this.state.isSelected ){
-              source.style.height = this.state.normalSize
-              source.style.width = this.state.normalSize              
+              source.style.height = this.state.normalSize;
+              source.style.width = this.state.normalSize ;             
               this.setState({
                 isSelected: false
-              })
+              });
             }
           }, 10) 
       )
-    })
+    });
   
   }
   ///////////////////////////////////////////////////////////////////////
   restoreCameoSize(eventObject){
     
-    if (this.state.wasClicked){return}
+	this.state.nameHolder.innerText = "Family Tree";
     const source = eventObject.target;
     
     //alert(eventObject.target.id)
-    source.style.height = this.state.normalSize
-    source.style.width = this.state.normalSize
+    source.style.height = this.state.normalSize;
+    source.style.width = this.state.normalSize;
     this.setState({
       isSelected: false
-    })    
+    });    
   }
   
 //==========================| RENDER |=========================//
 	render() {
 		
-		let name = this.props.contact.name;
-		let portraitURL = this.props.contact.portraitURL;
+		let name = this.props.info.nickName ? this.props.info.nickName : this.props.info.name ;
+		let portraitURL = this.props.info.portraitURL;
 		let style = {
 			background: `url('${portraitURL}') no-repeat center`,
 			backgroundSize: `contain`,
 			backgroundColor: `lightblue`,
 			borderRadius:'50%',
-		}
+		};
 		
 		let nameStyle = {
 			background: "rgba(0, 0, 0, 0.5)",
@@ -81,22 +100,23 @@ class Cameo extends React.Component{
 			bottom: "-11rem",
 			position: "relative",
 			width:'100%',			
-		}
+		};
 		
 		/**
 		 * If the bubble is selected then make the size larger and center it.
+		 *				onMouseEnter={this.enlargeCameo}  
 		 */
 
 		return(
-			  
-			<div className="cameo" 
-        style={style} 
-        onMouseEnter={this.enlargeCameo}              
-        onMouseLeave={this.restoreCameoSize}        
-        id={this.props.id}>
-				<p style={nameStyle}></p>
-        
-			</div>
+			<div 
+				className="cameo" 	
+				style={style}
+				name={name}
+				id={this.props.id}				
+				onClick={this.enlargeCameo}
+				onMouseLeave={this.restoreCameoSize} 
+			/>   
+			
 		)
 	}
 }
--- components/FamilyContainer.js
+++ components/FamilyContainer.js
@@ -9,44 +9,98 @@ class FamilyContainer extends React.Component {
 			familyInfo: [
 				{
 					description: "",
-					email: "",
-					name: "Frank Smith",
+					email: "Frank@FS.me",
+					name: "Frank Sinatra",
+					nickName: ' " Chairman of the Board " ',
 					portraitURL: "./images/frank.jpg",
-          screenLocation: 5,          
+        			screenLocation: 5,          
 					phone: "",
 				},
 				{
 					description: "",
-					email: "",
+					email: "Jackie@JC.me",
 					name: "Jackie Chan",
+					nickName: '" Bruce Lee Jr. "',					
 					portraitURL: "./images/JackieChan.jpg",
-          screenLocation: 5,          
+        			screenLocation: 5,          
 					phone: "",
 				},
 				{
 					description: "",
-					email: "",
+					email: "Charlize@CT.me",
 					name: "Charlize Theron",
+					nickName: null,					
 					portraitURL:  "./images/CharlizeTheron.jpg",
-          screenLocation: 5,          
+        			screenLocation: 5,          
 					phone: "",
 				},
 				{
 					description: "",
-					email: "",
+					email: "Halley@HB.me",
 					name: "Halley Berry",
+					nickName: null,					
 					portraitURL: "./images/HalleyBerry.jpg",
-          screenLocation: 5,          
+        			screenLocation: 5,          
 					phone: "",
 				},				
 				{
 					description: "",
-					email: "",
+					email: "Liam@LN.me",
 					name: "Liam Neeson",
-					portraitURL: "images/LiamNeesonjpg.jpg",
-          screenLocation: 5,          
+					nickName: ' " The Kraken Master " ',					
+					portraitURL: "./images/LiamNeesonjpg.jpg",
+        			screenLocation: 5,          
 					phone: "",
 				},
+				//////////////////////////////////////////////////////////
+				{
+					description: "",
+					email: "Frank@FS.me",
+					name: "Frank Sinatra",
+					nickName: ' " Chairman of the Board " ',
+					portraitURL: "./images/frank.jpg",
+        			screenLocation: 5,          
+					phone: "",
+				},
+				{
+					description: "",
+					email: "Jackie@JC.me",
+					name: "Jackie Chan",
+					nickName: '" Bruce Lee Jr. "',					
+					portraitURL: "./images/JackieChan.jpg",
+        			screenLocation: 5,          
+					phone: "",
+				},
+				{
+					description: "",
+					email: "Charlize@CT.me",
+					name: "Charlize Theron",
+					nickName: null,					
+					portraitURL:  "./images/CharlizeTheron.jpg",
+        			screenLocation: 5,          
+					phone: "",
+				},
+				{
+					description: "",
+					email: "Halley@HB.me",
+					name: "Halley Berry",
+					nickName: null,					
+					portraitURL: "./images/HalleyBerry.jpg",
+        			screenLocation: 5,          
+					phone: "",
+				},				
+				{
+					description: "",
+					email: "Liam@LN.me",
+					name: "Liam Neeson",
+					nickName: ' " The Kraken Master " ',					
+					portraitURL: "./images/LiamNeesonjpg.jpg",
+        			screenLocation: 5,          
+					phone: "",
+				},				
+				//////////////////////////////////////////////////////////
+						
+				
         
 			],
 		}
@@ -56,19 +110,18 @@ class FamilyContainer extends React.Component {
   assignRandomLocation(){
     
   }
-  
-  
 
 	
 	renderCameos() {
-		
+		//		let name = this.props.info.nickName ? this.props.info.nickName : this.props.info.name ;
 		const cameos = [];
-		let contact = null;
-		
-		for( let i=0; i<5; i++ ) {
-			
-			contact = this.state.familyInfo[i];
-			cameos.push(<Cameo contact={contact} key={i} id={"i"+i} />);
+		let info = null;
+		let name = null;
+		let maxCount = this.state.familyInfo.length
+		for( let i = 0; i < maxCount; i++ ) {
+			info = this.state.familyInfo[i];
+			name = info.nickName ? info.nickName : info.name;
+			cameos.push( <Cameo info={info} key={i} id={"i" + i} name={name}/> );
 		}
 		
 		return cameos;
